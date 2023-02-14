90b70a66bf35d7742394a886d1b310197c5cd352
alexeifilimonov <nlifeaf@mail.ru>
2023-02-14 19:44:16 +0300

    feat: implement GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.guessValue = Math.round((this.max - this.min) / 2) + this.min;
+        return this.guessValue;
     }
 
     lower() {
-
+        this.max = this.guessValue
     }
 
     greater() {
-
+        this.min = this.guessValue
     }
 }
 
